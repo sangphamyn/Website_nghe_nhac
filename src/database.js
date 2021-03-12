be6b12c6e9c7a65c50c48dc22ba6ccddf7246408
Sang <sangphamyn313@gmail.com>
2021-03-12 16:03:12 +0700

    test 3

--- src/database.js
+++ src/database.js
@@ -3,10 +3,6 @@ var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect();
 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
-});
+console.log("sang");
 
 connection.end();
